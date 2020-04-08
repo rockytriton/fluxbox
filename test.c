982ec509c84f07715dcd1d04bd6cbd76038eb156
Rocky Pulley <rockypulley@Rockys-iMac-27.local>
2020-04-07 19:14:28 -0500

    more log

--- test.c
+++ test.c
@@ -1,12 +1,34 @@
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xresource.h>
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 
     Display *d = XOpenDisplay(argv[1]);
 
+    if (!d) {
+        printf("Failed to connect to display\n");
+        return -1;
+    }
+
     printf("DISP: %X\n", d);
+    int screenNum = DefaultScreen(d);
+    long bg = BlackPixel(d, screenNum);
+    long border = WhitePixel(d, screenNum);
+
+    int w = 200;
+    int h = 200;
+
+    Window win = XCreateSimpleWindow(d, DefaultRootWindow(d), 50, 50, w, h, 2, border, bg);
+
+    printf("WIN: %X\n", win);
+
+    sleep(5);
+
+    XCloseDisplay(d);
 
     return 0;
 
